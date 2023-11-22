6039d97264100645263b221493e0b90f87fb470b
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-11-22 21:20:25 +0000

    Initial commit

---
+++ HelloWorldNHSAPITest/HelloWorldNHSAPITest/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace HelloWorldAPITest
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string url =
+            "https://sandbox.api.service.nhs.uk/hello-world/hello/world";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseString = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseString);
+            Console.ReadLine();
+        }
+    }
+}
